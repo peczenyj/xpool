1d3edae149f6d5426cbdca4ca1cfc2c423858496
Tiago Peczenyj <tpeczenyj@weborama.com>
2024-06-26 11:03:14 +0200

    add onReset callbacks

--- pool.go
+++ pool.go
@@ -2,6 +2,8 @@ package xpool
 
 import "sync"
 
+var _ Pool[any] = (*sync.Pool)(nil)
+
 // Pool is a type-safe object pool interface.
 type Pool[T any] interface {
 	// Get fetch one item from object pool
@@ -9,7 +11,7 @@ type Pool[T any] interface {
 	Get() T
 
 	// Put return the object to the pull.
-	// It may reset the object before put it back to sync pool.
+	// It may reset the object bef	// poolWriter onReset callback (called: true, onGet: true)ore put it back to sync pool.
 	Put(object T)
 }
 
@@ -19,9 +21,15 @@ type Resetter interface {
 	Reset()
 }
 
+// OnResetCallback type.
+// Will be called with a true value if the value T isa Resetter and was called with success.
+type OnResetCallback func(called bool)
+
 // New is the constructor of an xpool.Pool.
 // Receives the constructor of the type T.
-func New[T any](ctor func() T) Pool[T] {
+func New[T any](
+	ctor func() T,
+) Pool[T] {
 	return &simplePool[T]{
 		ctor: ctor,
 		pool: new(sync.Pool),
@@ -31,7 +39,10 @@ func New[T any](ctor func() T) Pool[T] {
 // NewWithDefaultResetter is an alternative constructor of an xpool.Pool.
 // We will call the resetter callback before put the object back to the pool.
 // Be careful, the custom resetter must be thread safe.
-func NewWithResetter[T any](ctor func() T, resetter func(T)) Pool[T] {
+func NewWithResetter[T any](
+	ctor func() T,
+	resetter func(T),
+) Pool[T] {
 	return &resettablePool[T]{
 		pool:     New(ctor),
 		resetter: resetter,
@@ -40,10 +51,19 @@ func NewWithResetter[T any](ctor func() T, resetter func(T)) Pool[T] {
 
 // NewWithDefaultResetter is an alternative constructor of an xpool.Pool.
 // If T is a Resetter, before put the object back to object pool we will call Reset().
-func NewWithDefaultResetter[T any](ctor func() T) Pool[T] {
+// It accepts optional callbacks to be executed after a Reset.
+func NewWithDefaultResetter[T any](
+	ctor func() T,
+	onResets ...OnResetCallback,
+) Pool[T] {
 	return NewWithResetter(ctor, func(t T) {
-		if defaultResetter, ok := any(t).(Resetter); ok {
+		defaultResetter, ok := any(t).(Resetter)
+		if ok {
 			defaultResetter.Reset()
+		}
+
+		for _, onReset := range onResets {
+			onReset(ok)
 		}
 	})
 }
